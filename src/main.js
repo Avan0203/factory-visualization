bd031ee49bded60a0c610dc257b30798cefca2e3
wuyifan <wuyifan@udschina.com>
2025-10-20 14:25:23 +0800

    chore: update author information and add console logs for debugging in main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 /*
  * @Author: wuyifan 1208097313@qq.com
  * @Date: 2025-04-17 00:59:57
- * @LastEditors: wuyifan 1208097313@qq.com
- * @LastEditTime: 2025-06-05 12:01:01
+ * @LastEditors: wuyifan wuyifan@udschina.com
+ * @LastEditTime: 2025-10-20 14:24:21
  * @FilePath: /factory-visualization/src/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,7 +12,11 @@ import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-import router from './router'
+import router from './router';
+
+console.log('i am running');
+
+console.log('base: ', import.meta.env.BASE_URL);
 
 const app = createApp(App)
 app.use(ElementPlus)
